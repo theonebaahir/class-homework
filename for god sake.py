63c7f400d02321642473048d76479dc6b25fda05
theonebaahir <babusadudekula@gmail.com>
2025-04-18 21:20:50 +0530

    Add files via upload

---
+++ for god sake.py
@@ -0,0 +1,18 @@
+a = ("12345")
+b = ("12345")
+c=a
+print(a is not c)
+# Mam I have a doubt inthie code Ia m writing below
+x = 123
+v = 45
+print(x and v) 
+p = "hello"
+y = "hello world"
+
+print(p in y  )
+print(p not in y  )
+a = 5
+b = 3
+print(a & b)
+print( "a << 1 :", a << 1)
+print( "a >> 1 :", a >> 1)
